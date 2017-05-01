a9bda3d4a08c846f10a1c66fff2d753b3f048874
Victor <x89168419357@gmail.com>
2017-05-01 23:41:54 +0300

    CustomEvent polyfill for IE

--- sort-test.js
+++ sort-test.js
@@ -1,4 +1,28 @@
-﻿var sortTest = (function() {
+﻿// CustomEvent polyfill for IE
+(function () {
+    if (typeof window.CustomEvent === 'function') {
+        return false;
+    }
+
+    function CustomEvent (event, params) {
+        params = params || {
+            bubbles: false,
+            cancelable: false,
+            detail: undefined
+        };
+
+        var evt = document.createEvent('CustomEvent');
+        evt.initCustomEvent(event, params.bubbles, params.cancelable, params.detail);
+        return evt;
+    }
+
+    CustomEvent.prototype = window.Event.prototype;
+
+    window.CustomEvent = CustomEvent;
+})();
+
+
+var sortTest = (function() {
     var currentTest = null,
         worker = null;
 
